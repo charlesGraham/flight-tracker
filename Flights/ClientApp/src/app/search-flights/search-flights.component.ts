172448e662007caf0da6d0490710f9e7faa7ef2b
Charles <15911636+charlesGraham@users.noreply.github.com>
2023-09-26 10:01:51 -0500

    Marked form & added types for flight search results & departure/arrival information

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
   styleUrls: ['./search-flights.component.css']
 })
-export class SearchFlightsComponent {
+export class SearchFlightsComponent implements OnInit {
 
+
+  searchResults: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: {
+        time: Date.now().toString(),
+        place: "Los Angeles",
+      },
+      arrival: {
+        time: Date.now().toString(),
+        place: "Tokyo"
+      },
+      price: "650"
+    },
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: {
+        time: Date.now().toString(),
+        place: "Munchen",
+      },
+      arrival: {
+        time: Date.now().toString(),
+        place: "Shiphol"
+      },
+      price: "800"
+    },
+    {
+      airline: "British Airways",
+      remainingNumberOfSeats: 60,
+      departure: {
+        time: Date.now().toString(),
+        place: "London",
+      },
+      arrival: {
+        time: Date.now().toString(),
+        place: "Vizzola-Tricino"
+      },
+      price: "730"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
