e205e580c5f40fb0ea3ac676b0929a91afe81163
Charles <15911636+charlesGraham@users.noreply.github.com>
2023-09-26 14:32:32 -0500

    Used swagger.json file to auto-generate APIs, models, services, etc. for Angular FE

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Time } from '@angular/common';
+import { FlightService } from '../api/services';
+import { FlightRm } from '../api/models';
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
@@ -8,49 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class SearchFlightsComponent implements OnInit {
 
 
-  searchResults: FlightRm[] = [
-    {
-      airline: "American Airlines",
-      remainingNumberOfSeats: 500,
-      departure: {
-        time: Date.now().toString(),
-        place: "Los Angeles",
-      },
-      arrival: {
-        time: Date.now().toString(),
-        place: "Tokyo"
-      },
-      price: "650"
-    },
-    {
-      airline: "Deutsche BA",
-      remainingNumberOfSeats: 60,
-      departure: {
-        time: Date.now().toString(),
-        place: "Munchen",
-      },
-      arrival: {
-        time: Date.now().toString(),
-        place: "Shiphol"
-      },
-      price: "800"
-    },
-    {
-      airline: "British Airways",
-      remainingNumberOfSeats: 60,
-      departure: {
-        time: Date.now().toString(),
-        place: "London",
-      },
-      arrival: {
-        time: Date.now().toString(),
-        place: "Vizzola-Tricino"
-      },
-      price: "730"
-    }
-  ];
+  searchResults: FlightRm[] = [];
 
-  constructor() { }
+  constructor(private flightService: FlightService) { }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
@@ -58,15 +20,3 @@ export class SearchFlightsComponent implements OnInit {
 
 }
 
-export interface FlightRm {
-  airline: string;
-  arrival: TimePlaceRm;
-  departure: TimePlaceRm;
-  price: string;
-  remainingNumberOfSeats: number;
-}
-
-export interface TimePlaceRm {
-  place: string;
-  time: string;
-}
